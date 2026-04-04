e48e30955ec87505258dc86bffb9c1f773d59cda
Authoi234 <jawadkhan.authoi@gmail.com>
2026-04-04 23:32:43 +0600

    stylish button added

--- components/ui-items/Specialbutton.js
+++ components/ui-items/Specialbutton.js
@@ -1,12 +1,27 @@
 'use client';
 import '../../app/globals.css';
 import { motion } from 'framer-motion';
-export default function SpecialButton({text}) {
+
+const myStyle = {
+  color: "#cfd8ff",
+  textShadow: `
+    0 0 6px #0029fa,
+    0 0 20px #001a9e,
+    0 0 30px #4a008f,
+    0 0 50px #5f00cc,
+    0 0 80px #a100ff
+  `,
+  filter: "brightness(1.7) saturate(1.5)",
+};
+export default function SpecialButton({ text }) {
   return (
-    <motion.button className="frutiger-button" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-      <div className="inner">
-        <div className="top-white"></div>
-        <span className="text">{text}</span>
+    <motion.button className="special_button" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+      <div class="wrap">
+        <p>
+          <span style={myStyle}>✧</span>
+          <span style={myStyle}>✦</span>
+          {text}
+        </p>
       </div>
     </motion.button>
   );
